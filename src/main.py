cb50971b7cc81f03169b82faf071bcd2005486f9
MIKAIL7787 <tort101to@gmail.com>
2026-06-22 23:15:51 +0300

    feat: add task journal script

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from helpers import load_tasks, print_tasks 
+
+def main():
+tasks = load_tasks("data/tasks.txt")
+print("=== Student Task Journal ===")
+print_tasks(tasks)
+if __name__ == "__main__":
+main()
